94dc67310557a12a091099270c93304527dd9c1b
RobertPlaiasu <robertplaiasu03@gamil.com>
2020-07-06 02:05:21 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var count = 0;
+
+function add() {
+	document.getElementById("count").innerHTML = count;
+	count++;
+}
+
+function minus() {
+	document.getElementById("count").innerHTML = count;
+	count--;
+}
